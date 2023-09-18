c65730274e7a15b3c8c2b488fe863bf014c9aa04
Elena <elena161284@mail.ru>
2023-09-18 12:39:27 +0300

    Commit finish

--- src/main/java/pro/sky/StringList/StringListApplication.java
+++ src/main/java/pro/sky/StringList/StringListApplication.java
@@ -1,13 +1,26 @@
 package pro.sky.StringList;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class StringListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StringListApplication.class, args);
-	}
+		StringList stringList = new StringListImpl();
+		stringList.add("test1"); // 0
+		stringList.add("test2"); // 1
+		stringList.add("test3"); // 2
+		stringList.add("test4"); // 3
 
+		stringList.add(3, "test6");
+
+		stringList.remove(1);
+
+		System.out.println(Arrays.toString(stringList.toArray()));
+
+		stringList.clear();
+		System.out.println(stringList.size());
+	}
 }
